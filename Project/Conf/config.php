6ce5f6a1f28adba2b6f01503664a57d4997f6904
JunhaoHo <122047397@qq.com>
2015-05-18 10:44:51 +0800

    safe

--- Project/Conf/config.php
+++ Project/Conf/config.php
@@ -3,13 +3,13 @@ return array(
 	    //'配置项'=>'配置值'
 		'DB_TYPE'   => 'mysql', // 数据库类型
 
-		'DB_HOST'   => '119.29.22.179', // 服务器地址
+		'DB_HOST'   => '', // 服务器地址
 
-		'DB_NAME'   => 'scap', // 数据库名
+		'DB_NAME'   => '', // 数据库名
 
-		'DB_USER'   => 'root', // 用户名
+		'DB_USER'   => '', // 用户名
 
-		'DB_PWD'    => 'CWVhQeE9PkYnCB', // 密码 tae
+		'DB_PWD'    => '', // 密码 tae
 
 		'DB_PORT'   => 3306, // 端口
 
